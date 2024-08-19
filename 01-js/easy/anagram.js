0c0f7c9f7aa2ded2c813b83c213ca1f91eb9327c
Sayak Pradhan <pradhansayak2002@gmail.com>
2024-08-19 21:20:13 +0530

    anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,22 @@
 */
 
 function isAnagram(str1, str2) {
+  if (str1.length != str2.length) {
+    return false;
+  }
 
+  let sortedstr1 = str1.split('').sort().join('');
+  let sortedstr2 = str2.split('').sort().join('');
+
+  return sortedstr1 === sortedstr2;
 }
+
+if(isAnagram("rain", "nair")) {
+  console.log("Anagram");
+}
+else {
+  console.log("Not an anagram");
+}
+
 
 module.exports = isAnagram;
